25453224b4e4746064fd9ae0e032bef923b04093
JoelJimenezDAW <joeljimenez.saureu@gmail.com>
2021-02-10 12:13:32 +0100

    main

---
+++ code/loader.js
@@ -0,0 +1,5 @@
+var contenedor_loader = document.getElementById('contenedor_carga'); 
+setTimeout(function() {
+    contenedor_loader.getElementsByClassName.opacity = 0 
+    contenedor_loader.classList.add('cerrar'); 
+}, 1500);
